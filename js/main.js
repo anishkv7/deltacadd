ee24a2aecb89773d3542004abd6d39fcb8e74788
Anusha002 <anusha002@gmail.com>
2018-03-25 16:11:51 +0530

    reading data from external json

--- js/main.js
+++ js/main.js
@@ -2,12 +2,27 @@ $(function(){
 	var date = new Date();
 
 	$("#header").load("include/header.html?v=" + date.getMilliseconds() ); 
+	
+
+	$.getJSON("../data/index.json", function(data) {
+		var carousel = $('#carouselT').html();
+  		$('#myslider').html(Mustache.to_html(carousel, data));
+
+  		var featurelist = $('#featurelistT').html();
+  		$('.featurelist').html(Mustache.to_html(featurelist, data));
+
+  		simpleslider.getSlider({
+	      container: document.getElementById('myslider'),
+	      init: -100,
+	      show: 0,
+	      end: 100,
+	      unit: '%'
+	    });
+
+
+
+	});
+
 });
 
-simpleslider.getSlider({
-      container: document.getElementById('myslider'),
-      init: -100,
-      show: 0,
-      end: 100,
-      unit: '%'
-    });
+
