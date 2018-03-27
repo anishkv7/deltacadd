8ddb4f2b579720f73988b0f29a1e23e12946cf8e
Anusha002 <anusha002@gmail.com>
2018-03-27 20:39:55 +0530

    documan page

--- js/main.js
+++ js/main.js
@@ -4,11 +4,17 @@ $(function(){
 	$("#header").load("include/header.html?v=" + date.getMilliseconds() ); 
 	
 	$("#footer").load("include/footer.html?v=" + date.getMilliseconds() ); 
+
+	var id = $("body").attr("id");
+	console.log("../data/" + id + ".json");
 	
-	$.getJSON("../data/index.json", function(data) {
+	$.getJSON("../data/" + id + ".json", function(data) {
+
+/*	$.getJSON("../data/index.json", function(data) { */
 
 		$('[type=x-tmpl-mustache]').each(function(i,val){
 			var tmplId = $(val).attr('id');
+			console.log(tmplId);
 			var tmpl = $(val).html();
 			console.log(Mustache.to_html(tmpl, data))
 			$('#' + tmplId.substring(0, tmplId.length-1) ).html(Mustache.to_html(tmpl, data));
