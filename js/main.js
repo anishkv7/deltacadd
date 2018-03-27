3451fa98784a20dfc476de9bd266823392f25b5d
Anish Vishwanathan <anish@zovi.com>
2018-03-27 07:02:44 +0530

    optimizing templating

--- js/main.js
+++ js/main.js
@@ -6,30 +6,21 @@ $(function(){
 	$("#footer").load("include/footer.html?v=" + date.getMilliseconds() ); 
 	
 	$.getJSON("../data/index.json", function(data) {
-		var carousel = $('#carouselT').html();
-  		$('#myslider').html(Mustache.to_html(carousel, data));
 
-  		var featurelist = $('#featurelistT').html();
-  		$('.featurelist').html(Mustache.to_html(featurelist, data));
+		$('[type=x-tmpl-mustache]').each(function(i,val){
+			var tmplId = $(val).attr('id');
+			var tmpl = $(val).html();
+			console.log(Mustache.to_html(tmpl, data))
+			$('#' + tmplId.substring(0, tmplId.length-1) ).html(Mustache.to_html(tmpl, data));
+		})
 
-  		var clients = $('#clientsT').html();
-  		$('.clients').html(Mustache.to_html(clients, data));
-
-  		var testimonials = $('#testimonialsT').html();
-  		$('.testimonials').html(Mustache.to_html(testimonials, data));
-
-  		var products = $('#productsT').html();
-  		$('.products').html(Mustache.to_html(products, data));
-
-
-  		simpleslider.getSlider({
-	      container: document.getElementById('myslider'),
+		simpleslider.getSlider({
+	      container: document.getElementById('carousel'),
 	      init: -100,
 	      show: 0,
 	      end: 100,
 	      unit: '%'
 	    });
-
 
 
 	});
