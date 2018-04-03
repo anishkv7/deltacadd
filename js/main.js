12ff831eab40360a082c9100bf4d091fa1b0a138
Anusha002 <anusha002@gmail.com>
2018-04-03 07:06:25 +0530

    documan request memo done

--- js/main.js
+++ js/main.js
@@ -2,13 +2,19 @@ $(function(){
 	var date = new Date();
 
 	$("#header").load("include/header.html?v=" + date.getMilliseconds() , function(){
-		 	$('.hamburger, .topnav').on('click', function() { 
-	        $('.topnav, .hamburger').toggleClass('fs');
-	        //$('.hamburger').text() == "☰" ? $('.hamburger').text('✕') : $('.hamburger').text('☰');
-	    });
+		 	$('.hamburger').on('click', function() { 
+		        $('.topnav, .hamburger').toggleClass('fs');
+		        $(".wrapper").toggleClass("menupadding");
+		       
+		    });
+
+		    $(".dropbtn").on('click',function(){
+		    	$("#submenu").toggle();
+
+		    });
 	}); 
 	
-	$("#footer").load("include/footer.html?v=" + date.getMilliseconds() ); 
+	$("#footer").load("include/footer.html?v=" + date.getMilliseconds()); 
 
 	var id = $("body").attr("id");
 	console.log("../data/" + id + ".json");
@@ -35,13 +41,37 @@ $(function(){
 		    });
 		}
 
-		
+
 
 
 	});
+
+	$(document).on('click', '.close, .requestmodal', function() {
+		$('.modal').toggle();
+	})
 
 
 
 });
 
+function postData() {
+	$('#sendinginfo').toggle();
+	var url = " https://script.google.com/macros/s/AKfycbxMs_yf2IW5DGsPZ232HJixWjJCVO-vcEo2YSzgXdqyHqCde00/exec";
+	url += "?Name=" + $("#fname").val() + "&Phone=" + $("#phonenumber").val()
+    $.get(url, function(data, status){
+   //     alert("Data: " + data + "\nStatus: " + status);
+   		if($("#contactform").hasClass("modal-content")){
+   			
+   			$('.container').html('<h3>Thank you for your interest. We will get back to you soon!</h3>');
+   		}else{
+
+   			$("#message").html('<h3>Thank you for your interest. We will get back to you soon!</h3>');
+   			$('#sendinginfo').toggle();
+   			$("#contactform")[0].reset();
+   			setTimeout(function(){ $("#message").html('') }, 4000);
+   		}
+
+    });
+    return false;
+}
 
